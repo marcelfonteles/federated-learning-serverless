c1db28a2926365d517914286aecdf588a4837c40
marcelfvieira <marcel.vieira@smartfit.com>
2023-05-04 20:43:56 -0300

    fix scripts

--- server_ow_rpi/deploy_all.sh
+++ server_ow_rpi/deploy_all.sh
@@ -1,12 +1,23 @@
- cd server_ow_rpi/functions/get_clients_to_train
- ./deploy.sh
- cd ../get_data
- ./deploy.sh
- cd ../get_model
- ./deploy.sh
- cd ../send_model
- ./deploy.sh
- cd ../start_training
- ./deploy.sh
- cd ../subscribe
- ./deploy.sh
+export OPENWHISK_HOME=/home/pi/Workspace/incubator-openwhisk
+export OPENWHISK_TMP_DIR=$OPENWHISK_HOME/tmp
+export PATH=$PATH:$OPENWHISK_HOME/bin
+alias wsk='wsk -i'
+
+cd server_ow_rpi/functions/get_clients_to_train
+chmod +x deploy.sh
+./deploy.sh
+cd ../get_data
+chmod +x deploy.sh
+./deploy.sh
+cd ../get_model
+chmod +x deploy.sh
+./deploy.sh
+cd ../send_model
+chmod +x deploy.sh
+./deploy.sh
+cd ../start_training
+chmod +x deploy.sh
+./deploy.sh
+cd ../subscribe
+chmod +x deploy.sh
+./deploy.sh
