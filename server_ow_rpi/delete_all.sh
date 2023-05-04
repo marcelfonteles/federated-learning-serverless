16da342a4fb22b7d8d44667353039310d0ceba1f
marcelfvieira <marcel.vieira@smartfit.com>
2023-05-04 20:39:22 -0300

    add scripts

---
+++ server_ow_rpi/delete_all.sh
@@ -0,0 +1,6 @@
+wsk action delete get_clients_to_train
+wsk action delete get_data
+wsk action delete get_model
+wsk action delete send_model
+wsk action delete start_training
+wsk action delete subscribe
---
+++ server_ow_rpi/deploy_all.sh
@@ -0,0 +1,12 @@
+ cd server_ow_rpi/functions/get_clients_to_train
+ ./deploy.sh
+ cd ../get_data
+ ./deploy.sh
+ cd ../get_model
+ ./deploy.sh
+ cd ../send_model
+ ./deploy.sh
+ cd ../start_training
+ ./deploy.sh
+ cd ../subscribe
+ ./deploy.sh
