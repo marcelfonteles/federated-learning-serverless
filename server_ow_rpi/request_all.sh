075a2c822e41005b05db0734971d82b3ea2b4bc4
marcelfvieira <marcel.vieira@smartfit.com>
2023-05-05 09:22:29 -0300

    fix scripts

---
+++ server_ow_rpi/request_all.sh
@@ -0,0 +1,48 @@
+export OPENWHISK_HOME=/home/pi/Workspace/incubator-openwhisk
+export OPENWHISK_TMP_DIR=$OPENWHISK_HOME/tmp
+export PATH=$PATH:$OPENWHISK_HOME/bin
+alias wsk='wsk -i'
+
+cd ./functions/get_clients_to_train
+chmod +x request.sh
+./request.sh
+
+echo 'sleeping...'
+sleep 8
+echo 'sleeping...[OK]'
+
+cd ../get_data
+chmod +x request.sh
+./request.sh
+
+echo 'sleeping...'
+sleep 8
+echo 'sleeping...[OK]'
+
+cd ../get_model
+chmod +x request.sh
+./request.sh
+
+echo 'sleeping...'
+sleep 8
+echo 'sleeping...[OK]'
+
+cd ../send_model
+chmod +x request.sh
+./request.sh
+
+echo 'sleeping...'
+sleep 8
+echo 'sleeping...[OK]'
+
+cd ../start_training
+chmod +x request.sh
+./request.sh
+
+echo 'sleeping...'
+sleep 8
+echo 'sleeping...[OK]'
+
+cd ../subscribe
+chmod +x request.sh
+./request.sh
